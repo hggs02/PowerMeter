9520f5f75aac0ac3153a93ff34ffdddd302e7b63
Sarath M <sarathm00@gmail.com>
2015-08-02 22:55:10 +0530

    Bugs remaining

--- web/static/ajax/fetch_from_db.php
+++ web/static/ajax/fetch_from_db.php
@@ -1,13 +1,15 @@
 <?php 
     $hostname_connection = "localhost"; 
     $database_connection = "dredger"; 
-    $username_connection = "admin"; 
+    $username_connection = "root"; 
     $password_connection = "aaggss"; 
     $connection = mysql_connect($hostname_connection, $username_connection, $password_connection) or trigger_error(mysql_error(),E_USER_ERROR); 
     mysql_select_db($database_connection,$connection) or die( mysql_error("could not connect to database! " ) ) ; 
 ?>
 
-<?php $display ="select * from db order by time desc limit 1"; 
+<?php 
+    $dredger_name = 'dredger1';
+    $display = sprintf("select * from db order by time desc limit 1"); 
     $result=mysql_query($display,$connection) or die(mysql_error()); 
     if($result == FALSE) 
       { die(mysql_error()); }
@@ -16,7 +18,7 @@
 <?php 
     $rows = mysql_fetch_assoc($result);
     $return_data=array('time' => $rows['time'],
-        'storage_tank_level'  => $rows['storage_tank_level'],
+        'storage_tank_level'  => $rows['consumption'],
         'storage_tank_cap'    => $rows['storage_tank_cap'],
         'service_tank_level'  => $rows['service_tank_level'],
         'service_tank_cap'    => $rows['service_tank_cap'],
