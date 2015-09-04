3f45bd2aa60c82f5052b6b7147e6b6616fc8d96f
Hari Govind <hggs02@gmail.com>
2015-09-04 16:23:24 +0530

    Debugging. .

--- PLC_delta.py
+++ PLC_delta.py
@@ -10,7 +10,7 @@ import random
 
 class Xbee(object):
     def __init__ (self):
-        self.obj = serial.Serial('/dev/ttyUSB1',9600,timeout=1)
+        self.obj = serial.Serial('/dev/ttyUSB0',9600,timeout=1)
     
     def xbee_write(self,data):
         self.obj.write(data)
@@ -18,7 +18,6 @@ class Xbee(object):
 
     def xbee_read(self):
         while True:
-            #val = self.obj.read(100).decode('ascii').strip()
             val = self.obj.read(100).strip()
             if val:
                 return val
@@ -40,8 +39,8 @@ if __name__ == '__main__':
     while True:
         try:
              
-            """
-            instrument = minimalmodbus.Instrument('/dev/port1',1)
+            
+            instrument = minimalmodbus.Instrument('/dev/ttyUSB0',1)
             #instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1, minimalmodbus.MODE_ASCII)
             instrument.serial.baudrate = 9600
             instrument.serial.bytesize = 8
@@ -51,9 +50,9 @@ if __name__ == '__main__':
             #instrument.debug='false'
             instrument.mode = minimalmodbus.MODE_ASCII
             #print instrument
-            """
+            
             time  = strftime("%Y-%m-%d %H:%M:%S")
-            #consumption  = instrument.read_float(4201)/100000
+            #consumption  = instrument.read_register(4209)
             consumption = dummyPacket()
 
             
