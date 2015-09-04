2f917b4b1da7e93352519b42e1245726ad42de67
Sarath M <sarathm00@gmail.com>
2015-09-04 20:12:32 +0530

    Code Working for Pwermeter (Little genius+ 1119)

--- PLC_delta.py
+++ PLC_delta.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 
 import minimalmodbus
 import serial
@@ -12,7 +12,7 @@ import random
 class Xbee(object):
     
     def __init__ (self):
-        self.obj = serial.Serial('/dev/xb1',9600,timeout=1)
+        self.obj = serial.Serial('/dev/ttyUSB0',9600,timeout=1)
     
     def xbee_write(self,data):
         self.obj.write(data)
@@ -29,8 +29,8 @@ class PLC():
     def __init__(self):
         try:
             self.instrument = minimalmodbus.Instrument('/dev/plc',1)
-            self.instrument.serial.baudrate = 9600
-            self.instrument.serial.bytesize = 8
+            self.instrument.serial.baudrate = 19200
+            self.instrument.serial.bytesize = 7
             self.instrument.serial.parity = serial.PARITY_EVEN
             self.instrument.serial.stopbits = 1
             self.instrument.serial.timeout = 0.1
@@ -39,24 +39,25 @@ class PLC():
             #self.instrument.debug='false'
             #print self.instrument
 
-        except Exception, e:
+        except Exception as e:
+           # print 'plc init():'+str(e)
             print 'class PLC __init__(): %s' %(str(e),)
         
         
-    def read_int():
-        data  = self.instrument.read_register(4209)
+    def read_int(self):
+        data  = self.instrument.read_register(5146)
         return data
     
-    def read_float():
-        data  = self.instrument.read_float(4201)/100000
+    def read_float(self):
+        data  = self.instrument.read_float(5147)/100000
         return data
 
 
-db = database_class()
+db = database_class() 
 
 if __name__ == '__main__':
 
-    xb = Xbee()
+    #xb = Xbee()
     plc = PLC()
     
     print('\t\tPOWER CONSUMPTION')
@@ -64,21 +65,20 @@ if __name__ == '__main__':
     while True:
         try:
             time  = strftime("%Y-%m-%d %H:%M:%S")
-            #consumption  = plc.read_int()
-            consumption = dummyPacket()
-
+            #consumption = dummyPacket()
+            consumption  = plc.read_float()
             
             print('-------------------------------------------------')
             print '|',time,'\tConsumption => \t',consumption,'Unit','|','\n'
             packet = str(time)+';'+str(consumption)
             db.insertDb(time,consumption)
-            xb.xbee_write(packet)
+            #xb.xbee_write(packet)
             
-            sleep(3)
+            sleep(30)
             
         except Exception as e:
             print(e)
-            sleep(1)
+            sleep(30)
     
    
         
